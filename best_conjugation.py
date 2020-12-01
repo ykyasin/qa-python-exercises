d860b602070cda1c919ac7977286bf23bb690e7a
Yusuf Yasin <yyasin@hotmail.co.uk>
2020-12-01 11:07:50 +0000

    uploaded word text file, started on the best_conjugation ex but still incomplete

---
+++ best_conjugation.py
@@ -0,0 +1,24 @@
+word_file = open("qa-python-exercises\wordlist.txt","r")
+
+wordlist = []
+for line in word_file:
+  stripped_line = line.strip()
+  line_list = stripped_line.split()
+  wordlist.append(line_list)
+
+word_file.close()
+
+yourword = input("input your word: ")
+
+def conjugation(yourword):
+    checklist = []
+    for i in wordlist:
+        i = "".join(i)
+        if i in yourword:
+            checklist.append(i)
+    return "Sub words that make up your word: " + ", ".join(checklist)
+
+print(conjugation(yourword))
+
+    
+
